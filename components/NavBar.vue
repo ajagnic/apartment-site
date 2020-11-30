0fb1ec66b8fa40cd4b2086dcd604b20d9bc5a59d
ajagnic <adrian.agnic@hotmail.com>
2020-11-30 11:33:54 -0500

    working nav links

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -5,11 +5,19 @@
     </v-app-bar>
     <v-navigation-drawer v-model="drawer" app>
       <v-list nav>
-        <v-list-item-group>
-          <v-list-item>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
+        <v-list-item
+          v-for="page in pages"
+          :key="page.title"
+          nuxt
+          :to="page.link"
+        >
+          <v-list-item-icon>
+            <v-icon v-text="page.icon" />
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="page.title" />
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
   </div>
@@ -19,6 +27,7 @@
 export default {
   data: () => ({
     drawer: null,
+    pages: [{ title: 'Home', icon: 'mdi-home', link: '/' }],
   }),
 }
 </script>
