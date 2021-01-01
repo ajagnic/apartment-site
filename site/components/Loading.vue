1d8dfd8208330af47889effaf1313346187921e6
ajagnic <adrian.agnic@hotmail.com>
2020-12-31 19:30:42 -0500

    custom loading spinner

---
+++ site/components/Loading.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-overlay v-if="loading" :value="overlay">
+    <v-progress-circular indeterminate size="64" />
+  </v-overlay>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    loading: false,
+    overlay: false,
+  }),
+
+  methods: {
+    start() {
+      this.loading = true
+      this.overlay = true
+    },
+    finish() {
+      this.loading = false
+      this.overlay = false
+    },
+  },
+}
+</script>
