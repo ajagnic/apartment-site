ec0b8792774f607a04cb861bebfcc119ec08f747
ajagnic <adrian.agnic@hotmail.com>
2021-01-02 09:26:12 -0500

    db connect/disconnect

--- api/db/client.go
+++ api/db/client.go
@@ -10,19 +10,39 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-func init() {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://admin:root@mongodb:27017"))
-	if err != nil {
-		fmt.Printf("client:%v\n", err)
-	}
+var (
+	client *mongo.Client
+)
+
+// Connect attempts to connect to the mongodb instance.
+func Connect(user, pw string) (err error) {
+	uri := fmt.Sprintf("mongodb://%s:%s@mongodb:27017", user, pw)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	err = client.Connect(ctx)
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI(uri))
 	if err != nil {
-		fmt.Printf("conn:%v\n", err)
+		return
 	}
-	err = client.Ping(ctx, readpref.Primary())
-	if err != nil {
-		fmt.Printf("ping:%v\n", err)
+	err = ping(ctx)
+	return
+}
+
+// Disconnect should be called when the client is no longer needed.
+func Disconnect() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := client.Disconnect(ctx); err != nil {
+		panic(err)
 	}
+}
+
+func ping(ctx context.Context) (err error) {
+	for i := 0; i < 10; i++ {
+		err = client.Ping(ctx, readpref.Primary())
+		if err == nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+	return
 }
--- api/main.go
+++ api/main.go
@@ -4,19 +4,27 @@ import (
 	"log"
 	"os"
 
-	_ "github.com/ajagnic/apartment-site/db"
+	"github.com/ajagnic/apartment-site/db"
 	"github.com/ajagnic/apartment-site/server"
 )
 
 func main() {
+	user := os.Getenv("MONGO_INITDB_ROOT_USERNAME")
+	pw := os.Getenv("MONGO_INITDB_ROOT_PASSWORD")
+	err := db.Connect(user, pw)
+	if err != nil {
+		log.Fatalf("Could not connect to database: %v\n", err)
+	}
+
 	host := os.Getenv("API_HOST")
 	port := os.Getenv("API_PORT")
 	addr := host + ":" + port
 
-	log.Printf("Starting server on %s", addr)
-	err := server.Run(addr)
+	log.Printf("Starting server on %s\n", addr)
+	err = server.Run(addr)
 	if err != nil {
-		log.Printf("Server error: %v", err)
+		log.Printf("Server error: %v\n", err)
 	}
-	log.Printf("Server gracefully shutdown")
+
+	db.Disconnect()
 }
