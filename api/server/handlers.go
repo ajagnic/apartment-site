349959983f93f616ea03bc2e2efae81089697d09
ajagnic <adrian.agnic@hotmail.com>
2021-01-07 11:47:25 -0500

    http get conditional for reservationhandler

--- api/server/handlers.go
+++ api/server/handlers.go
@@ -7,24 +7,41 @@ import (
 	"github.com/ajagnic/apartment-site/db"
 )
 
+const (
+	reservationsTable = "reservations"
+)
+
 func reservationHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 
-	if r.Method == http.MethodOptions {
+	if r.Method == http.MethodOptions { //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 		w.Header().Add("Access-Control-Allow-Headers", "*")
 		w.WriteHeader(http.StatusOK)
-	} else {
+
+	} else if r.Method == http.MethodGet { //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		b, err := db.CollectDates(reservationsTable)
+		if err != nil {
+			http.Error(w, "Error retrieving records.", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write(b)
+
+	} else if r.Method == http.MethodPost { //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 		b, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			http.Error(w, "Could not read request body.", http.StatusBadRequest)
 			return
 		}
-		err = db.Insert("reservations", b)
+		err = db.Insert(reservationsTable, b)
 		if err != nil {
 			http.Error(w, "Error saving new record.", http.StatusInternalServerError)
 			return
 		}
 		w.WriteHeader(http.StatusAccepted)
+		w.Write([]byte("Reservation created."))
+
+	} else {
+		http.Error(w, "Invalid method.", http.StatusMethodNotAllowed)
 	}
-	w.Write([]byte("OK"))
 }
