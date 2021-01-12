b36a1b94140f1f46f42ea3631ea9092a39034e00
ajagnic <adrian.agnic@hotmail.com>
2021-01-12 10:41:31 -0500

    add confirm and cancel fields to reservation type

--- api/db/types.go
+++ api/db/types.go
@@ -2,13 +2,15 @@ package db
 
 // Reservation represents the form submitted by the ReservationForm component.
 type Reservation struct {
-	Name            string
-	Phone           string
-	Email           string
-	Apartment       string
-	Guests          string
-	Dates           []string
-	ReservationDate string
+	Name      string
+	Phone     string
+	Email     string
+	Apartment string
+	Guests    int
+	Dates     []string
+	Created   string
+	Confirmed bool
+	Cancelled bool
 }
 
 // Result contains reserved dates for a single record, used by CollectDates.
