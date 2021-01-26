2484a7cb93b4f1f09e29d701d253c8b2811e48a7
ajagnic <adrian.agnic@hotmail.com>
2021-01-26 08:14:12 -0500

    parse html template. gen email body

--- api/email/service.go
+++ api/email/service.go
@@ -1,7 +1,8 @@
 package email
 
 import (
-	"fmt"
+	"bytes"
+	"html/template"
 	"net/smtp"
 )
 
@@ -11,23 +12,38 @@ var (
 	from string
 )
 
+// TemplateData stores values to be injected into HTML.
+type TemplateData struct {
+	URL string
+}
+
 // Initialize authentication values for the smtp client.
 func Initialize(host, port, sender, password string) {
-	addr = host + ":" + port
 	from = sender
+	addr = host + ":" + port
 	auth = smtp.PlainAuth("", sender, password, host)
 }
 
-// SendConfirmation sends content as an email to all recipients.
-func SendConfirmation(id string, to string) error {
-	recipients := []string{to}
-	body := fmt.Sprintf("Reservation ID: %s\r\n", id)
-	msg := []byte("From: Agnic Apartments\r\n" +
-		"Subject: Confirm Your Reservation\r\n" +
-		"\r\n" + body)
-	err := smtp.SendMail(addr, auth, from, recipients, msg)
+// SendConfirmation emails a confirmation link to the recipient.
+func SendConfirmation(id string, to string) (err error) {
+	msg, err := generateEmailBody(id)
 	if err != nil {
-		return err
+		return
 	}
-	return nil
+	recipients := []string{to}
+	err = smtp.SendMail(addr, auth, from, recipients, msg)
+	return
+}
+
+func generateEmailBody(id string) ([]byte, error) {
+	var body bytes.Buffer
+	mimeHeaders := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	body.Write([]byte("From: Agnic Apartments\r\nSubject: Confirm Your Reservation\r\n" + mimeHeaders))
+	t, err := template.ParseFiles("./email/template.html")
+	if err != nil {
+		return nil, err
+	}
+	url := "http://localhost:3000/confirmation?id=" + id
+	t.Execute(&body, TemplateData{url})
+	return body.Bytes(), nil
 }
