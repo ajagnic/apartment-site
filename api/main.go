2c37552b636174160f6dd402ff330493bc26e9e6
ajagnic <adrian.agnic@hotmail.com>
2021-01-28 09:48:46 -0500

    move envars to server pkg, minor cmt and refactoring

--- api/main.go
+++ api/main.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/ajagnic/apartment-site/db"
+	_ "github.com/ajagnic/apartment-site/email"
 	"github.com/ajagnic/apartment-site/server"
-)
-
-var (
-	host = os.Getenv("API_HOST")
-	port = os.Getenv("API_PORT")
 )
 
 func main() {
@@ -19,10 +14,7 @@ func main() {
 		log.Fatalf("Could not connect to database: %v\n", err)
 	}
 
-	addr := host + ":" + port
-
-	log.Printf("Starting server on %s\n", addr)
-	err = server.Run(addr)
+	err = server.Run()
 	if err != nil {
 		log.Printf("Server error: %v\n", err)
 	}
